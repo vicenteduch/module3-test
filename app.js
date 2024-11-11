06cf7f5a7034659f5222638363ab1a5a0af4cc72
Vicente Duch <vhduch@gmail.com>
2024-11-11 12:28:12 +0100

    small tests

--- app.js
+++ app.js
@@ -1,41 +1,71 @@
+
 const express = require("express");
+const logger = require("morgan");
+
+const data = require("./data/pizzas.js")
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 3000
+
+
+// setup the request logger to run on each request (Morgan)
+app.use(logger("dev"));
 
 // Make the static files inside of the 'public/' folder 
 app.use(express.static("public"));
+
+// JSON middleware to parse incoming HTTP requests that contain JSON
+app.use(express.json());
 
 //app.get(path, code);
 //when you define the code, that's gonna be a function automatically receiving 3 things:
 //a request -> an object with a lot of properties and methods, so big.
 //check documentation from expresjs related to requests, response and next
 
-app.get("/", function(request, response, next){ 
-  //  console.log(request);
-  // console.log(request.method);
-  // console.log(request.hostname);
-    response.send('<h1>This is the homepage</h1> <img src="/images/home.jpg"/>')
+app.use(function(req, res, next){
+    console.log("Hello World 1 ...");
+    next();
+});
+
+//GET / (home page)
+
+app.get("/", function (request, response, next) {
+    // console.log(request);
+    // console.log(request.method);
+    // console.log(request.hostname);
+    response.sendFile(__dirname + "/views/home.html")
     console.log("we received a GET request for the HOME page...")
 });
 
+
+//GET /contact
 // request, response and next usually are used as req, res and next
 
-app.get("/contact", function(req, res, next){
+app.get("/contact", function (req, res, next) {
     //console.log("we received a GET request for the CONTACT page...")
-    res.send('<h1> This is the contact page </h1>')
+    res.sendFile(__dirname + "/views/contact.html")
 });
 
-app.get("/about", function(req, res, next){
+//GET /about
+
+app.get("/about", function (req, res, next) {
     //console.log("we received a GET request for the ABOUT page")
     res.send("This is the about page")
 })
 
-app.get("*", function(req, res, next){
+//GET /pizzas
+
+app.get("/pizzas", function(req, res, next){
+   res.json(data);
+})
+
+//GET page not found
+
+app.get("*", function (req, res, next) {
     console.log("page not found")
     res.send("404 PAGE NOT FOUND")
 })
 
-app.listen(PORT, function(){ console.log(`Server listening on port ${PORT}...`)});
+app.listen(PORT, function () { console.log(`Server listening on port ${PORT}...`) });
 
---
+++ data/pizzas.js
@@ -0,0 +1,29 @@
+const pizzasArr = [
+    {
+        id: 1,
+        title: 'pizza margarita',
+        price: 5,
+        imageFile: 'pizza-margarita.jpg'
+    },
+    {
+        id:2,
+        title: 'pizza carbonara' ,
+        price: 20,
+        imageFile: 'pizza-carbonara.jpg'
+    },
+    {
+        id: 3,
+        title: 'pizza BBQ',
+        price: 15,
+        imageFile: 'pizza-carbonara.jpg'
+    },
+    {
+        id: 4,
+        title: 'four cheese',
+        price: 0.50,
+        imageFile: 'pizza-four-cheese.jpg'
+    }
+];
+
+
+module.exports = pizzasArr;
