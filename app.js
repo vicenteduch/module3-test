1faffeef2a964abdd9a965a66d50b73b96a9db0c
Vicente Duch <vhduch@gmail.com>
2024-11-11 11:24:16 +0100

    add img to homepage

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 
 const PORT = 3000;
 
+// Make the static files inside of the 'public/' folder 
+app.use(express.static("public"));
+
 //app.get(path, code);
 //when you define the code, that's gonna be a function automatically receiving 3 things:
 //a request -> an object with a lot of properties and methods, so big.
@@ -13,17 +16,15 @@ app.get("/", function(request, response, next){
   //  console.log(request);
   // console.log(request.method);
   // console.log(request.hostname);
-    response.send("This is the homepage")
+    response.send('<h1>This is the homepage</h1> <img src="/images/home.jpg"/>')
     console.log("we received a GET request for the HOME page...")
 });
 
 // request, response and next usually are used as req, res and next
 
-
-
 app.get("/contact", function(req, res, next){
     //console.log("we received a GET request for the CONTACT page...")
-    res.send("<h1>This is the contact page<h1><h2>subtitle<h2>")
+    res.send('<h1> This is the contact page </h1>')
 });
 
 app.get("/about", function(req, res, next){
