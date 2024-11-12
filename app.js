648b38fb9b9a83cfaad51966a2157e18ec484a31
Vicente Duch <vhduch@gmail.com>
2024-11-12 10:56:49 +0100

    add example of req.params

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const logger = require("morgan");
 
-const data = require("./data/pizzas.js")
+const data = require("./data/pizzas.js");
+const pizzasArr = require("./data/pizzas.js");
 
 const app = express();
 
@@ -23,46 +24,52 @@ app.use(express.json());
 //a request -> an object with a lot of properties and methods, so big.
 //check documentation from expresjs related to requests, response and next
 
-
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     console.log("Hello World 1 ...");
     next();
 });
 
 //GET / (home page)
-
 app.get("/", function (request, response, next) {
     // console.log(request);
     // console.log(request.method);
     // console.log(request.hostname);
+
     response.sendFile(__dirname + "/views/home.html")
     console.log("we received a GET request for the HOME page...")
 });
 
-
 //GET /contact
 // request, response and next usually are used as req, res and next
-
 app.get("/contact", function (req, res, next) {
     //console.log("we received a GET request for the CONTACT page...")
     res.sendFile(__dirname + "/views/contact.html")
 });
 
 //GET /about
-
 app.get("/about", function (req, res, next) {
     //console.log("we received a GET request for the ABOUT page")
     res.send("This is the about page")
 })
 
 //GET /pizzas
-
-app.get("/pizzas", function(req, res, next){
-   res.json(data);
+app.get("/pizzas", function (req, res, next) {
+    res.json(data);
 })
 
-//GET page not found
+//GET pizzas by id
+app.get("/pizzas/:pizzaId", function (req, res, next) {
 
+    const id = req.params.pizzaId;
+
+    const pizzaDetails = pizzasArr.find((pizzaObj) => {
+        return pizzaObj.id == id;
+    });
+
+    res.json(pizzaDetails);
+});
+
+//GET page not found
 app.get("*", function (req, res, next) {
     console.log("page not found")
     res.send("404 PAGE NOT FOUND")
