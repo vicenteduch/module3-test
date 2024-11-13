ee4e9d7e9f1123de921205072d10df503f6907f8
Vicente Duch <vhduch@gmail.com>
2024-11-13 11:59:52 +0100

    implement endpoint to Create new pizzas

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const data = require("./data/pizzas.js");
 const pizzasArr = require("./data/pizzas.js");
+const Pizza = require("./models/Pizza.model");
 
 const app = express();
 
 const PORT = 3000
 
 
-// setup the request logger to run on each request (Morgan)
+// Setup the request logger to run on each request (Morgan)
 app.use(logger("dev"));
 
 // Make the static files inside of the 'public/' folder 
@@ -18,6 +20,19 @@ app.use(express.static("public"));
 
 // JSON middleware to parse incoming HTTP requests that contain JSON
 app.use(express.json());
+
+
+
+//connect to DB
+mongoose.connect("mongodb://127.0.0.1:27017/demo-restaurant")
+    .then((response) => {
+        console.log("conected!")
+    })
+    .catch((error) => {
+        console.log("error...")
+        console.log(error)
+    })
+
 
 //app.get(path, code);
 //when you define the code, that's gonna be a function automatically receiving 3 things:
@@ -52,19 +67,35 @@ app.get("/about", function (req, res, next) {
     res.send("This is the about page")
 })
 
+//POST /pizzas
+app.post("/pizzas", (req, res, next) => {
+
+const newPizza = req.body;
+
+     Pizza.create(newPizza)
+         .then(pizzaFromDB => {
+             res.status(201).json(pizzaFromDB);
+         })
+        .catch(error => {
+             console.log("error...");
+             console.log(error);
+            res.status(500).json({ error: "Failed to create a new pizza" });
+        })
+})
+
 //GET /pizzas
 //GET /pizzas?maxPrice=15
 app.get("/pizzas", function (req, res, next) {
 
     const maxPrice = req.query.maxPrice;
     // if maxPrice is not provided, return the whole list
-    if(maxPrice === undefined){
+    if (maxPrice === undefined) {
         res.json(data);
         return
     }
     // if maxPrice is privided, return only the pizzas 
-    const result =  data.filter( (pizzaObj) => {
-      return pizzaObj.price <= maxPrice;
+    const result = data.filter((pizzaObj) => {
+        return pizzaObj.price <= maxPrice;
     });
 
     res.json(result);
